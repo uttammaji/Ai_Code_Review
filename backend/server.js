34b6f97a9bdb24019f71755689ac442b31f85e3b
Uttam Maji <uttammaji842@gmail.com>
2026-08-07 23:51:23 +0530

    Port update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 connectDB()
     .then(() => {
-        app.listen(PORT, () => {
+        app.listen(PORT, "0.0.0.0",() => {
             console.log(`Server running on port http://localhost:${PORT}`);
         });
     })
